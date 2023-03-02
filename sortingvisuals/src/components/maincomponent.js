793738f2cc7a06593b8872fd25f1fb0236920462
Jared Lee <115897648+untamedduck@users.noreply.github.com>
2023-03-02 12:03:22 -0700

    styling

--- sortingvisuals/src/components/maincomponent.js
+++ sortingvisuals/src/components/maincomponent.js
@@ -13,8 +13,8 @@ class Main extends React.Component {
     //create a new array with random values
     resetArray() {
         const array = [];
-        for(let i = 0; i<100;i++){
-            array.push(randomIntFromInterval(5,700));
+        for(let i = 0; i<75;i++){
+            array.push(randomIntFromInterval(5,800));
         }
         this.setState({array});
     }
@@ -22,14 +22,21 @@ class Main extends React.Component {
     render() {
         const{array} = this.state;
         return (
+            <div className= "w-full flex h-100 overflow-hidden items-end">
+                <div className="SIDEBAR w-1/5 min-h-screen flex-col bg-gradient-to-t from-[#1D0E23] via-[#5E4D7A] to-[#B57BEE]">  
+                    <header> Sorting Algorithim Visualiser</header>
+                    <button onClick={()=>this.resetArray()} className = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"> reset </button>
+                    <button className = "bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"> sort </button>
 
-            <div className="w-100 h-96 justify-center items-end">
+                </div>
+            <div className="ARRAY CONTAINER w-4/5 h-screen items-end flex justify-evenly">
             {array.map((value,idx)=>(
                 <div 
-                    className="w-2 bg-red inline-flex px-1 m-1" 
+                    className=" BARS w-30 bg-blue-900 flex px-1 m-1 flex" 
                     key = {idx}
                     style={{height: `${value}px`}}></div>
             ))}
+            </div>
             </div>
             
         );
